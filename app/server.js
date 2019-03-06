591a3495c4557c440d09a0ae5baa549d6e44192e
AnonymousICT <kuohoward@gmail.com>
2019-03-06 13:02:26 -0800

    added user model

---
+++ app/server.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const { PORT, HTTP_STATUS_CODES, MONGO_URL, TEST_MONGO_URL } = require('./config');
+// const { authRouter } = require('./auth/auth.router');
+// const { userRouter } = require('./user/user.router');
+// const { propertyRouter } = require('./property/property.router');
+// const { localStrategy, jwtStrategy } = require('./auth/auth.strategy');
+
+let server;
+const app = express(); 
+// passport.use(localStrategy); 
+// passport.use(jwtStrategy); 
+
+// MIDDLEWARE
+app.use(morgan('combined')); 
+app.use(express.json()); 
+app.use(express.static('./public')); 
+
+// ROUTER SETUP
+// app.use('/api/auth', authRouter); 
+// app.use('/api/user', userRouter); 
+// app.use('/api/property', propertyRouter); 
+
+
+app.use('*', function (req, res) {
+    res.status(HTTP_STATUS_CODES.NOT_FOUND).json({ error: 'Not Found.' });
+});
+
+module.exports = {
+    app,
+    startServer,
+    stopServer
+};
+
+function startServer(testEnv) {
+    return new Promise((resolve, reject) => {
+        let mongoUrl;
+
+        if (testEnv) {
+            mongoUrl = TEST_MONGO_URL;
+        } else {
+            mongoUrl = MONGO_URL;
+        }
+        mongoose.connect(mongoUrl, { useNewUrlParser: true }, err => {
+            if (err) {
+                console.error(err);
+                return reject(err);
+            } else {
+                server = app.listen(PORT, () => {
+                    console.log(`Express server listening on http://localhost:${PORT}`);
+                    resolve();
+                }).on('error', err => {
+                    mongoose.disconnect();
+                    console.error(err);
+                    reject(err);
+                });
+            }
+        });
+    });
+}
+
+function stopServer() {
+    return mongoose
+        .disconnect()
+        .then(() => new Promise((resolve, reject) => {
+            server.close(err => {
+                if (err) {
+                    console.error(err);
+                    return reject(err);
+                } else {
+                    console.log('Express server stopped.');
+                    resolve();
+                }
+            });
+        }));
+}
